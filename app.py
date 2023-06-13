f0755ec331e52d811ed7750c2c1117d1741cf42b
hessdale <hessdale96@gmail.com>
2023-06-12 19:29:48 -0600

    finished writing code and working on debugging

--- app.py
+++ app.py
@@ -2,11 +2,66 @@ import json
 import mariadb
 import dbcreds
 import dbhelper
+import uuid
 from flask import Flask, request, make_response, jsonify
 app = Flask(__name__)
 
+@app.post('/api/client')
+def new_client():
+    try:
+        error = dbhelper.check_endpoint_info([request.json.get("username"),request.json.get("password")],["username","password"])
+        if(error != None):
+            return make_response(jsonify(error),400)
+        results = dbhelper.run_procedure('call new_client(?,?)',[request.json.get("username"),request.json.get("password")])
+        if(type(results) == list):
+            return make_response(jsonify(results),200)
+        else:
+            return make_response('sorry something went wrong',500)
+    except TypeError:
+        print('invalid input type, try again.')
+    except UnboundLocalError:
+        print('coding error')
+    except ValueError:
+        print('value error, try again')
 
 
+@app.post('/api/login')
+def login():
+    try:
+        error = dbhelper.check_endpoint_info([request.json.get("username"),request.json.get("password")],["username","password"])
+        if(error != None):
+            return make_response(jsonify(error),400)
+        token = uuid.uuid4().hex
+        results = dbhelper.run_procedure('call login(?,?,?)',[request.json.get("username"),request.json.get("password"),token])
+        if(type(results) == list):
+            return make_response(jsonify(results),200)
+        else:
+            return make_response('sorry something went wrong',500)
+    except TypeError:
+        print('invalid input type, try again.')
+    except UnboundLocalError:
+        print('coding error')
+    except ValueError:
+        print('value error, try again')
+
+
+@app.post('/api/post')
+def new_post():
+    try:
+        error = dbhelper.check_endpoint_info([request.json.get("content")],["content"])
+        if(error != None):
+            return make_response(jsonify(error),400)
+        results = dbhelper.run_procedure('call new_post(?,?)',[request.json.get("content"),request.json.get("token")])
+        if(type(results) == list):
+            return make_response(jsonify(results),200)
+        else:
+            return make_response('sorry something went wrong',500)
+    except TypeError:
+        print('invalid input type, try again.')
+    except UnboundLocalError:
+        print('coding error')
+    except ValueError:
+        print('value error, try again')
 
 
 
